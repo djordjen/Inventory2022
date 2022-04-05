39c6801b7f587bd31ac8af3ec74f522ea85f73ad
djordjen <djordje@najdanovic.com>
2022-04-05 10:16:13 +0200

    Before upgrading DX Components

---
+++ Inventory2022.Module/BusinessObjects/Document.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using DevExpress.Xpo;
+using DevExpress.ExpressApp;
+using System.ComponentModel;
+using DevExpress.ExpressApp.DC;
+using DevExpress.Data.Filtering;
+using DevExpress.Persistent.Base;
+using System.Collections.Generic;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+
+namespace Inventory2022.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    [ImageName("BO_Document")]
+    [DefaultProperty("Number")]
+    [ModelDefault("IsCloneable", "True")]
+    [OptimisticLocking(false), DeferredDeletion(false)]
+    [DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    public class Document : XPObject
+    {
+        public Document(Session session) : base(session) { }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+
+        }
+
+        DateTime date;
+        string number;
+
+        [Size(10)]
+        [NonCloneable]
+        [RuleRequiredField, RuleUniqueValue]
+        [Indexed(Name = "IX_Number", Unique = true)]
+        public string Number
+        {
+            get { return number; }
+            set { SetPropertyValue("Number", ref number, value); }
+        }
+
+        [NonCloneable]
+        [DbType("smalldatetime")]
+        [VisibleInLookupListView(true)]
+        public DateTime Date
+        {
+            get { return date; }
+            set { SetPropertyValue("Date", ref date, value); }
+        }
+
+
+
+    }
+}
---
+++ Inventory2022.Module/Enums/DocumentTypeEN.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory2022.Module.Enums
+{
+    /// <summary>
+    /// Simple list
+    /// </summary>
+    public enum DocumentTypeEN : int
+    {
+        Entry = 0,
+        Discharge = 1,
+        Balance = 2
+    }
+    
+    /*   Comprehensive List:
+     * 
+     * - Purchase Order     = 0
+     * - Sales Quotation    = 1
+     * - Sales Order        = 2
+     * - Delivery Order     = 3
+     * - Invoice            = 4
+     * - Credit Note        = 5
+     * - Physical Inventory = 6
+     * - Write Off          = 7
+     * - Return             = 8
+     * - Internal Delivery  = 9
+     * - Internal Receipt   = 10
+     * - Discharge          = 11
+     */ 
+}
